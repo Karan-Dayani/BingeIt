1a46e306a04f6cac3f6be90cf2fa160a6295d0f0
Karan <karandayani39@gmail.com>
2024-01-31 17:15:56 +0530

    made navbar responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link, NavLink } from "react-router-dom";
+import MenuRoundedIcon from '@mui/icons-material/MenuRounded';
+import { pink } from "@mui/material/colors";
 import "./header.css"
 
 export default function Header() {
+    const [menu, setMenu] = useState("menu")
+
+    const handleMenuToggle = () => {
+        setMenu(prevClass => prevClass === "menu" ? "menu open" : "menu")
+    }
+
     return (
         <header>
             <div className="pc-header">
                 <Link className="brand" to="/">BINGEIT</Link>
                 <ul>
                     <li>
-                        <NavLink>MOVIES</NavLink>
+                        <NavLink to="/movies">MOVIES</NavLink>
                     </li>
                     <li>
-                        <NavLink>TV SHOWS</NavLink>
+                        <NavLink to="/tv-shows">TV SHOWS</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/watchlist">WATCHLIST</NavLink>
+                    </li>
+                </ul>
+            </div>
+            <div className="mob-header">
+                <Link className="brand" to="/">BINGEIT</Link>
+                <MenuRoundedIcon onClick={handleMenuToggle} sx={{ fontSize: 50, color: "var(--hover)" }} />
+            </div>
+            <div className={menu}>
+                <ul>
+                    <li>
+                        <NavLink to="/movies">MOVIES</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/tv-shows">TV SHOWS</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/watchlist">WATCHLIST</NavLink>
                     </li>
                 </ul>
             </div>
