32902e16e31c3336a75863ccdafd8262098ba063
Karan <karandayani39@gmail.com>
2024-02-03 18:52:52 +0530

    added popular-tv-shows

--- src/api.js
+++ src/api.js
@@ -8,3 +8,14 @@ export async function getPopularMovies(page) {
         throw error        
     }
 }
+
+export async function getPopularTvShows(page) {
+    const url = `https://api.themoviedb.org/3/tv/popular?api_key=${import.meta.env.VITE_API_KEY}&language=en-US&page=${page}`;
+    try {
+        const res = await fetch(url);
+        const data = res.json();
+        return data
+    } catch (error) {
+        throw error        
+    }
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,14 @@
 import React from "react";
-import { getPopularMovies } from "../api";
+import { getPopularMovies, getPopularTvShows } from "../api";
 import { Await, defer, useLoaderData } from "react-router-dom";
 import { Suspense } from "react";
 import CardSlider from "../components/CardSlider";
 
 export function loader() {
-    return defer({ popularMovies: getPopularMovies(1) })
+    return defer({
+        popularMovies: getPopularMovies(1),
+        PopularTvShows: getPopularTvShows(1)
+    })
 }
 
 export default function Home() {
@@ -16,6 +19,11 @@ export default function Home() {
                 <Await resolve={data.popularMovies}>
                     {(popularMovies) => (
                         <CardSlider data={popularMovies.results} title={"Popular Movies"} seeMore={"/movies"} />
+                    )}
+                </Await>
+                <Await resolve={data.PopularTvShows}>
+                    {(PopularTvShows) => (
+                        <CardSlider data={PopularTvShows.results} title={"Popular Tv Shows"} seeMore={"/tv-shows"} />
                     )}
                 </Await>
             </Suspense>
